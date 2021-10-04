07b3cb9ef74fe5c94aa40ee016b5b002d7ee4e8d
123 <123@K218PC1>
2021-10-04 14:04:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2; int b = 3; int c = 4;
+            //Console.Write(Treug(2, 3, 4));
+            if (Treug(a, b, c))
+                Console.WriteLine($"Периметр треугольник{PeriTreug(a, b, c)}");
+            Console.WriteLine($"Площадь треугольника{SquaTreug(a, b, c)}");
+        }
+        static bool Treug(int a, int b, int c)
+        { if ((a + b > c) && (a + c > b) && (c + b > a))
+                return true;
+            else
+                return false;
+        }
+        static int PeriTreug(int a,int b,int c)
+        {
+            return a + b + c;
+        }
+        static Double SquaTreug(int a,int b,int c)
+        {
+            int ppp = PeriTreug(a, b, c);
+            return Math.Sqrt(ppp * (ppp - a) * (ppp - b) * (ppp - c))/4;
+        }
+    }
+}
